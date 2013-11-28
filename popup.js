bce1546781d9f6493ff8b802623047e6cd25c180
hagward <hagward@kth.se>
2013-11-28 23:26:39 +0900

    working version + todo added

--- popup.js
+++ popup.js
@@ -1,31 +1,3 @@
-/*
-1 2 3 4 5 6 7 8 9 0
-0 9 8 7 6 5 4 3 2 1
-1 2 3 4 5 6 7 8 9 0
-0 9 8 7 6 5 4 3 2 1
-1 2 3 4 5 6 7 8 9 0
-0 9 8 7 6 5 4 3 2 1
-1 2 3 4 5 6 7 8 9 0
-*/
-
-// matrix size is 10 x 7
-
-function stringToMatrix(str) {
-    /*
-    var tokens = str.split();
-
-    if (tokens.length != 70)
-        return null;
-
-    var m = [];
-    for (var i = 0; i < 7; i++) {
-        m.push([]);
-        for (var j = 0; j < 10; j++)
-            m[i].push(
-    return m;
-    */
-}
-
 function matrixToString(matrix) {
     var str = '';
     for (var i = 0; i < matrix.length; i++) {
